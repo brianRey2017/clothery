fc592363d5ba3c16a8c7a966fa55ba29521c92f0
Brian <brianrey3@protonmail.com>
2021-07-29 12:41:29 -0300

    Added state for user

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,26 +1,40 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import React from "react";
 
 import { ReactComponent as Logo } from "@images/crown.svg";
 import "./header.styles.scss";
+import { auth } from "@lib/firebase";
 
-const Header = () => (
-  <div className="header">
-    <Link to="/">
-      <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
+const Header = ({ currentUser }) => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <Logo className="logo" />
       </Link>
-      <Link className="option" to="">
-        CONTACT
-      </Link>
-      <Link className="option" to="/signin">
-        LOGIN
-      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+Header.propTypes = {
+  currentUser: PropTypes.oneOfType([PropTypes.object, PropTypes.oneOf([null])]),
+};
 
 export default Header;

cfe8a8dda71a03f17c09ca70740305e7b07e67a0
Brian <brianrey3@protonmail.com>
2021-08-04 12:22:27 -0300

    Added extra button to cart dropdown and styling

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,40 @@
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import React from "react";
 
+import { clearCart } from "@redux/cart/cart.actions";
 import CustomButton from "@components/common/custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems, clearCart, isCartEmpty }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items"></div>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
+      <div className="buttons">
+        {!isCartEmpty && (
+          <CustomButton variant="danger" onClick={() => clearCart()}>
+            CLEAR CART
+          </CustomButton>
+        )}
+        <CustomButton>GO TO CHECKOUT</CustomButton>
+      </div>
     </div>
   );
 };
 
-export default CartDropdown;
+CartDropdown.propTypes = {
+  clearCart: PropTypes.func,
+  cartItems: PropTypes.arrayOf([PropTypes.object]),
+  isCartEmpty: PropTypes.bool,
+};
+
+const mapStateToProps = ({ cart }) => ({
+  cartItems: cart.items,
+  isCartEmpty: cart.items.length === 0,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(clearCart(item)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartDropdown);

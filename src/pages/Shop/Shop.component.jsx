2b852c7c251ccc7e201bdb742f5bec0ccd540925
Brian <brianrey3@protonmail.com>
2021-08-09 12:48:35 -0300

    ADDED COLLECTION COMPONENTS SUBFOLDER

--- src/pages/Shop/Shop.component.jsx
+++ src/pages/Shop/Shop.component.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { SHOP_DATA } from "@data/shop";
-import CollectionPreview from "@components/collection-preview/collection-preview.component";
+import CollectionPreview from "@components/collection/collection-preview/collection-preview.component";
 
 export const Shop = () => {
   const [collections] = useState(SHOP_DATA);

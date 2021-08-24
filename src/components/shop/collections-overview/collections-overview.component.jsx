60bd0b37362c5d68cdae1a3ff41c2ccf91f3f056
Brian <brianrey3@protonmail.com>
2021-08-24 10:56:21 -0300

    Collection-overview component migrated

--- src/components/shop/collections-overview/collections-overview.component.jsx
+++ src/components/shop/collections-overview/collections-overview.component.jsx
@@ -4,18 +4,17 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import CollectionPreview from "@components/shop/collection-preview/collection-preview.component";
+import { CollectionSchema } from "@components/shop/collection/collection.schema";
+import { CollectionsOverviewContainer } from "./collections-overview.styles";
 import { selectShopCollectionsForPreview } from "@redux/shop/shop.selectors";
-
-import "./collections-overview.styles.scss";
-import { CollectionSchema } from "../collection/collection.schema";
 
 const CollectionsOverview = ({ collections }) => {
   return (
-    <div className="collections-overview">
+    <CollectionsOverviewContainer>
       {collections.map(({ id, ...collectionProps }) => (
         <CollectionPreview key={id} id={id} {...collectionProps} />
       ))}
-    </div>
+    </CollectionsOverviewContainer>
   );
 };
 
